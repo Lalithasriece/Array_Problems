264691ea50cc72a50fe30aa4acf273de70ef99fe
Lalithasriece <144588804+Lalithasriece@users.noreply.github.com>
2024-07-02 23:04:49 +0530

    Add files via upload

---
+++ MinMax.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class min {
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+        System.out.print("Enter the value:");
+for(int i=0;i<n;i++){
+    a[i]=sc.nextInt();
+}
+int min=a[0];
+for(int i=1;i<n;i++){
+    if(a[i]<min){
+        min=a[i];
+    }
+}
+System.out.println("The Minimum value is: "+min);
+
+
+int max=a[0];
+for(int i=1;i<n;i++){
+    if(a[i]>max){
+        max=a[i];
+    }
+}
+System.out.println("The Maximum value is: "+max);
+
+
+
+
+
+}
+}
